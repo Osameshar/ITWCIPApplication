45ec84b51c916f7e9b99a8ab4ec36ef4d70ddbd2
Osameshar <cakeller42@gmail.com>
2016-03-18 18:43:33 -0500

    BackButtonFix

--- VSApp/ITW_MobileApp.Droid/Activities/EventCreationActivity.cs
+++ VSApp/ITW_MobileApp.Droid/Activities/EventCreationActivity.cs
@@ -239,7 +239,8 @@ namespace ITW_MobileApp.Droid
                 _drawer.CloseDrawer(GravityCompat.Start);
             }
             else {
-                base.OnBackPressed();
+                var intent = new Intent(this, typeof(RecentEventsActivity));
+                StartActivity(intent);
             }
         }
     }
