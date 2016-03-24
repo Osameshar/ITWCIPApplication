dba4553a69d359fe4bb7a0514d7d1ed7c9716636
Osameshar <cakeller42@gmail.com>
2016-03-24 16:59:19 -0500

    AddFilesToiOS

---
+++ VSApp/ITW_MobileApp.iOS/ObjectFactorys/EmployeeFactory.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace ITW_MobileApp.Droid
+{
+    public class EmployeeFactory
+    {
+        public async void createEmployee(string newName, string newEmail, int newEmployeeID, string newDepartment, string newPrivledgeLevel)
+        {
+            await IoC.Dbconnect.getEmployeeSyncTable().InsertAsync(new EmployeeItem { Name = newName, Email = newEmail, EmployeeID = newEmployeeID, Department = newDepartment, PrivledgeLevel = newPrivledgeLevel});
+            await IoC.Dbconnect.SyncAsync();
+        }
+    }
+}
