4d02c58651fc1bb3e2c71e0949cb9d706c153c31
blacknumbers <clintregeon@charter.net>
2016-04-10 19:52:19 -0500

    Started work on the iOS interface. Definitely not finished. Problems with how the sidebar works. But, it works.

---
+++ VSApp/ITW_MobileApp.iOS/MenuController.cs
@@ -0,0 +1,20 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ITW_MobileApp.iOS
+{
+    partial class MenuController : BaseController
+    {
+        public MenuController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+        }
+    }
+}
