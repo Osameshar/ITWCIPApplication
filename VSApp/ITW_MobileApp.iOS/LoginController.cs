ef308f2ce0fee1f2ac381572ba8e7fc9a927e38d
blacknumbers <clintregeon@charter.net>
2016-04-11 17:48:05 -0500

    Redo of the interface. Still not working as expected, but getting closer.

---
+++ VSApp/ITW_MobileApp.iOS/LoginController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ITW_MobileApp.iOS
+{
+	partial class LoginController : UIViewController
+	{
+		public LoginController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
